27a1282c84ecc7538d2706c9e167e8146bf74776
Eva Schlinger <eschling@andrew.cmu.edu>
2013-08-14 18:34:40 -0400

    cython/python import

--- morphogen/structlearn/__init__.py
+++ morphogen/structlearn/__init__.py
@@ -1,2 +1,6 @@
-import pyximport; pyximport.install()
-from csgd import StructuredClassifier
+#TODO make into actual module
+try:
+  import pyximport; pyximport.install()
+  from csgd import StructuredClassifier
+except ImportError:
+  from sgd import StructuredClassifier
